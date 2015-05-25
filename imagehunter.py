6aa1e690b0ad2d55d7b0dd4c9c682c1245da3116
Michael Haggerty <mhagger@alum.mit.edu>
2015-05-25 11:55:38 +0200

    Initial revision

---
+++ imagehunter.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+
+import sys
+import urllib
+import urllib2
+import simplejson
+import os
+import pprint
+import random
+
+
+def choose_random_search_term():
+    SEARCH_WORD_LIST = 'searchoptions.txt'
+    #SEARCH_WORD_LIST = '/usr/share/dict/words'
+
+    search_terms = [
+        line.strip()
+        for line in open(SEARCH_WORD_LIST)
+        if line.strip()
+    ]
+
+    return random.choice(search_terms)
+
+
+if len(sys.argv) > 1:
+    search_term = ' '.join(sys.argv[1:])
+else:
+    search_term = choose_random_search_term()
+
+print 'Searching for %s' % (search_term,)
+
+url = ('https://ajax.googleapis.com/ajax/services/search/images?' +
+       urllib.urlencode(
+           dict(
+               v='1.0',
+               q=search_term,
+               userip='93.219.10.12',
+               as_filetype='jpg',
+           )
+       )
+   )
+
+print 'Search url: %s' % (url,)
+
+request = urllib2.Request(url, None, {'Referer': "softwareswirl.com"})
+response = urllib2.urlopen(request)
+
+# Process the JSON string.
+results = simplejson.load(response)
+
+#pprint.pprint(results)
+
+urls = [
+    result['url']
+    for result in results['responseData']['results']
+]
+
+url = random.choice(urls)
+
+print 'Image url: %s' % (url,)
+
+request = urllib2.Request(url, None, {'Referer': "softwareswirl.com"})
+response = urllib2.urlopen(request)
+
+picture = response.read()
+
+open('image.jpg', 'wb').write(picture)
+os.system('eog image.jpg &')
+
